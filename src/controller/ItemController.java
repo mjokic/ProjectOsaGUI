d534b6c57249438807fe29420bb3a97c7a7cb03e
Marko Jokic <markojokic1996@gmail.com>
2017-07-01 00:48:03 +0200

    Added item image upload

--- src/controller/ItemController.java
+++ src/controller/ItemController.java
@@ -6,13 +6,20 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import model.Manager;
 import model.converters.UserDTOConverter;
 import model.dto.ItemDTO;
 import model.dto.UserDTO;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Response;
 
+import java.io.File;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -25,6 +32,10 @@ public class ItemController implements Initializable {
     private Stage window;
     private ItemDTO item;
 
+    private String imageName = "default.png";
+
+    @FXML private ImageView imageViewItemImage;
+    @FXML private Button buttonBrowseImage;
     @FXML private TextField textFieldItemID;
     @FXML private TextField textFieldItemName;
     @FXML private TextArea textAreaItemDesc;
@@ -40,7 +51,10 @@ public class ItemController implements Initializable {
         comboBoxItemUser.getItems().addAll(usersList);
 
         if(item != null){
-//            window.setTitle("Item Edit");
+
+            this.imageName = item.getPicture();
+            loadImage(item.getPicture());
+
             textFieldItemID.setText(String.valueOf(item.getId()));
             textFieldItemName.setText(item.getName());
             textAreaItemDesc.setText(item.getDescription());
@@ -52,6 +66,7 @@ public class ItemController implements Initializable {
             }
 
         }else{
+            loadImage(this.imageName);
             buttonItemSave.setText("Add");
         }
 
@@ -76,15 +91,59 @@ public class ItemController implements Initializable {
 
     }
 
+    private void loadImage(String imageName){
+        String url = Manager.url + "/images/items/" + imageName;
+        imageViewItemImage.setImage(new Image(url));
+    }
+
+    public void choseImage(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Chose image");
+        fileChooser.getExtensionFilters()
+                .add(new FileChooser.ExtensionFilter("All Images", "*.jpg", "*.jpeg","*.png"));
+
+        File file = fileChooser.showOpenDialog(window);
+
+        if(file != null) {
+            System.out.println(file.getName());
+
+            boolean status;
+            try {
+                status = uploadImage(file);
+            }catch (Exception ex){
+                new Alert(Alert.AlertType.ERROR, ex.getMessage()).showAndWait();
+                return;
+            }
+
+            if(status){
+                loadImage(this.imageName);
+                new Alert(Alert.AlertType.INFORMATION, "Upload successful!").showAndWait();
+            }else{
+                new Alert(Alert.AlertType.ERROR, "Upload unsuccessful!").showAndWait();
+            }
+        }
+
+
+    }
+
+
     public void onButtonItemSave(){
+
+        ItemDTO itemDTO = new ItemDTO();
+        itemDTO.setName(textFieldItemName.getText());
+        itemDTO.setDescription(textAreaItemDesc.getText());
+        UserDTO selectedUser = (UserDTO) comboBoxItemUser.getValue();
+        itemDTO.setUserId(selectedUser.getId());
+        itemDTO.setPicture(this.imageName);
 
         try {
             if (item == null) {
                 // send create request
-                sendCreateRequest();
+                sendCreateRequest(itemDTO);
             } else {
                 // send edit request
-                sendEditRequest();
+                itemDTO.setId(item.getId());
+                sendEditRequest(itemDTO);
             }
         }catch (Exception ex){
             new Alert(Alert.AlertType.ERROR, ex.getMessage()).showAndWait();
@@ -95,14 +154,8 @@ public class ItemController implements Initializable {
 
     }
 
-    private boolean sendCreateRequest() throws Exception{
+    private boolean sendCreateRequest(ItemDTO itemDTO) throws Exception{
         boolean status = false;
-
-        ItemDTO itemDTO = new ItemDTO();
-        itemDTO.setName(textFieldItemName.getText());
-        itemDTO.setDescription(textAreaItemDesc.getText());
-        UserDTO selectedUser = (UserDTO) comboBoxItemUser.getValue();
-        itemDTO.setUserId(selectedUser.getId());
 
         Response response;
         try{
@@ -122,18 +175,13 @@ public class ItemController implements Initializable {
         return status;
     }
 
-    private boolean sendEditRequest() throws Exception{
+    private boolean sendEditRequest(ItemDTO itemDTO) throws Exception{
         boolean status = false;
-
-        item.setName(textFieldItemName.getText());
-        item.setDescription(textAreaItemDesc.getText());
-        UserDTO selectedUser = (UserDTO)comboBoxItemUser.getValue();
-        item.setUserId(selectedUser.getId());
 
         Response response;
         try{
             response = Manager.itemApiService
-                    .editItem(item, item.getId(), Manager.token)
+                    .editItem(itemDTO, itemDTO.getId(), Manager.token)
                     .execute();
         }catch (Exception ex){
             throw ex;
@@ -148,6 +196,34 @@ public class ItemController implements Initializable {
         return status;
     }
 
+    private boolean uploadImage(File file) throws Exception{
+        boolean status = false;
+
+        RequestBody requestFile =
+                RequestBody.create(MediaType.parse("multipart/form-data"), file);
+
+        // MultipartBody.Part is used to send also the actual file name
+        MultipartBody.Part body =
+                MultipartBody.Part.createFormData("file", file.getName(), requestFile);
+
+        Response response;
+        try {
+            response = Manager.userApiService.uploadItemPicture(body, Manager.token).execute();
+        }catch (Exception ex){
+            return status;
+        }
+
+        if(response.code() == 200){
+            status = true;
+            this.imageName = response.body().toString();
+            System.out.println(this.imageName);
+
+        }else{
+            throw new Exception(response.errorBody().string());
+        }
+
+        return status;
+    }
 
     public void setItem(ItemDTO item){
         this.item = item;
